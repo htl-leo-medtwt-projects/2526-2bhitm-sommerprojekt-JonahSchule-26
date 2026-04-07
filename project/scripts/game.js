84e02573b26806dbc5f3226c562c40bcd9ef9e72
Jonah Kalmbach <j.kalmbach@students.htl-leonding.ac.at>
2026-04-07 21:11:26 +0200

    Add garage functionality and enhance hub layout with lives and power display

---
+++ project/scripts/game.js
@@ -0,0 +1,59 @@
+const SCREEN = [
+    [
+        document.getElementById("hub"),
+        document.getElementById("hub-image"),
+        document.getElementById("player-box"),
+        document.getElementById("hub-elements"),
+        document.getElementById("goToGarage-button"),
+        document.getElementById("lives"),
+        document.getElementById("heart1"),
+        document.getElementById("heart2"),
+        document.getElementById("heart3"),
+        document.getElementById("garage-power"),
+        document.getElementById("power-text"),
+        document.getElementById("power-bar"),
+        document.getElementById("power-fill")
+    ],
+    [
+        document.getElementById("garage"),
+        document.getElementById("upgrade-grid"),
+        document.getElementById("motor"),
+        document.getElementById("motor-upgrade-text-box"),
+        document.getElementById("grip"),
+        document.getElementById("grip-upgrade-text-box"),
+        document.getElementById("transmisson"),
+        document.getElementById("transmission-upgrade-text-box"),
+        document.getElementById("img-of-kart"),
+        document.querySelector("#garage .back-button"),
+        document.querySelector("#garage .back-button button")
+    ]
+];
+
+function showHub() {
+    SCREEN[0].forEach(element => {
+        element.style.display = "block";
+    });
+    SCREEN[1].forEach(element => {
+        element.style.display = "none";
+    });
+}
+
+function showGarage() {
+    SCREEN[0].forEach(element => {
+        element.style.display = "none";
+    });
+    SCREEN[1].forEach(element => {
+        element.style.display = "block";
+    });
+}
+
+
+
+
+
+/* --------------------------
+    window.onload Functions
+-------------------------- */
+window.onload = function() {
+    showHub();
+};
