0ccb8edcfc688e741a28b85311886b2aefcf7697
Jonah Kalmbach <j.kalmbach@students.htl-leonding.ac.at>
2026-04-17 12:41:27 +0200

    Implement game loop and key event handling; adjust hub positioning and add .gitignore

--- project/scripts/game.js
+++ project/scripts/game.js
@@ -31,21 +31,99 @@ const SCREEN = [
   ],
 ];
 
-const UPGRADES = [
-  0, /* motor */
-  0, /* grip */
-  0, /* transmisson */
-]
+const UPGRADES = [0 /* motor */, 0 /* grip */, 0 /* transmisson */];
 
-const collectibles = 0;
+const GAME_CONFIG = {
+  gamespeed: 24,
+  characterSpeed: 5,
+};
 
+const KEY_EVENTS = {
+  leftArrow: false,
+  rightArrow: false,
+  upArrow: false,
+  downArrow: false,
+};
 
+document.onkeydown = keyListenerDown;
+document.onkeyup = keyListenerUp;
+
+function keyListenerDown(e) {
+  if (e.key === "ArrowLeft") {
+    // Left arrow
+    KEY_EVENTS.leftArrow = true;
+  }
+  if (e.key === "ArrowUp") {
+    // Up arrow
+    KEY_EVENTS.upArrow = true;
+  }
+  if (e.key === "ArrowRight") {
+    // Right arrow
+    KEY_EVENTS.rightArrow = true;
+  }
+  if (e.key === "ArrowDown") {
+    // Down arrow
+    KEY_EVENTS.downArrow = true;
+  }
+}
+
+function keyListenerUp(e) {
+  if (e.key === "ArrowLeft") {
+    // Left arrow
+    KEY_EVENTS.leftArrow = false;
+  }
+  if (e.key === "ArrowUp") {
+    // Up arrow
+    KEY_EVENTS.upArrow = false;
+  }
+  if (e.key === "ArrowRight") {
+    // Right arrow
+    KEY_EVENTS.rightArrow = false;
+  }
+  if (e.key === "ArrowDown") {
+    // Down arrow
+    KEY_EVENTS.downArrow = false;
+  }
+}
+
+function movePlayer(dx, dy, dr) {
+  // save original position
+  let originalX = parseFloat(SCREEN[0][1].style.right);
+  let originalY = parseFloat(SCREEN[0][1].style.bottom);
+
+  SCREEN[0][1].style.right = originalX + dx + "px";
+  SCREEN[0][1].style.bottom = originalY + dy + "px";
+
+  /*if (dr != 0 && dr != PLAYER.spriteDirection) {
+    PLAYER.spriteDirection = dr;
+    PLAYER.box.style.transform = `scaleX(${dr})`;
+  } */
+}
 
 /* --------------------------
     Hub Functions
 -------------------------- */
 
+function gameLoop() {
+  if (KEY_EVENTS.leftArrow) {
+    movePlayer(-1 * GAME_CONFIG.characterSpeed, 0, -1);
+    // animatePlayer();
+  }
+  if (KEY_EVENTS.rightArrow) {
+    movePlayer(GAME_CONFIG.characterSpeed, 0, 1);
+    // animatePlayer();
+  }
+  if (KEY_EVENTS.upArrow) {
+    movePlayer(0, -1 * GAME_CONFIG.characterSpeed, 0);
+    // animatePlayer();
+  }
+  if (KEY_EVENTS.downArrow) {
+    movePlayer(0, GAME_CONFIG.characterSpeed, 0);
+    // animatePlayer();
+  }
 
+  setTimeout(gameLoop, 1000 / GAME_CONFIG.gameSpeed);
+}
 
 /* --------------------------
     Screen Functions
@@ -89,10 +167,6 @@ function showGarage() {
   }
 }
 
-
-
-
-
 /* --------------------------
     Upgrade Functions
 -------------------------- */
@@ -103,7 +177,6 @@ function upgradeMotor() {
   } else {
     alert("Motor is already at max level!");
   }
-  
 }
 
 function upgradeGrip() {
@@ -131,10 +204,11 @@ function showUpgradeCarImg() {
   }, 1000);
 }
 
-
 /* --------------------------
     window.onload Functions
 -------------------------- */
 window.onload = function () {
   showHub();
 };
+
+  gameLoop();
